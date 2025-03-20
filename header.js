f72eaec6407de8d91def078cbd159bf567a36c99
thebondingproject <snsluca@gmail.com>
2025-03-20 16:15:47 +0100

    first commit

---
+++ header.js
@@ -0,0 +1,8 @@
+//Vedi "javascript - Include another HTML file in a HTML file - Stack Overflow.pdf"
+document.write('\
+<!-- Header -->\
+<header id="header">\
+	<a href="index.html" class="logo"><strong>The Bonding Project</strong></a>\
+	<a href="https://dpia.uniud.it/" class="logo icons">Polytechnic Department of the University of Udine <span class="icon solid fa-university"></span></a>\
+</header>\
+');
---
+++ sidebar_and_footer.js
@@ -0,0 +1,42 @@
+
+//Vedi "javascript - Include another HTML file in a HTML file - Stack Overflow.pdf"
+document.write('\
+<!-- Sidebar -->\
+<div id="sidebar">\
+	<div class="inner">\
+		<!-- Menu -->\
+			<nav id="menu">\
+				<header class="major">\
+					<h2>Menu</h2>\
+				</header>\
+				<ul>\
+					<li><a href="index.html">Homepage</a></li>\
+					<li><a href="project_overview.html">Project Overview</a></li>\
+					<li>\
+						<span class="opener">Development Pages</span>\
+						<ul>\
+        					<li><a href="page_template.html">Page Template</a></li>\
+        					<li><a href="elements.html">Elements</a></li>\
+						</ul>\
+					</li>\
+				</ul>\
+			</nav>\
+		<!-- Section -->\
+			<section>\
+				<header class="major">\
+					<h2>Get in touch</h2>\
+				</header>\
+				<ul class="contact">\
+					<li class="icon solid fa-envelope">danelutti.massimo(at)spes.uniud.it</li>\
+					<li class="icon solid fa-envelope">minigher.claudio(at)spes.uniud.it</li>\
+					<li class="icon solid fa-envelope">sanson.luca(at)spes.uniud.it</li>\
+					<li class="icon solid fa-home">Via delle Scienze, 206<br/>Udine, 33100, Italy</li>\
+				</ul>\
+			</section>\
+		<!-- Footer -->\
+			<footer id="footer">\
+				<p class="copyright">&copy; The Bonding Project, a lab project by the University of Udine. All rights reserved. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>\
+			</footer>\
+	</div>\
+</div>\
+');
