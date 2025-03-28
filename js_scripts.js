a09aba543bb063e6c9f11fa29e6a3c844af86923
thebondingproject <devs.thebondingproject@gmail.com>
2025-03-28 15:06:44 +0100

    enabled website zoom; centered images; expanded mechanical description and ultrasonic driver

---
+++ js_scripts.js
@@ -0,0 +1,9 @@
+//Vedi "javascript - Include another HTML file in a HTML file - Stack Overflow.pdf"
+document.write('\
+    <script src="assets/js/jquery.min.js"></script>\
+	<script src="assets/js/browser.min.js"></script>\
+	<script src="assets/js/breakpoints.min.js"></script>\
+	<script src="assets/js/util.js"></script>\
+	<script src="assets/js/main.js"></script>\
+	<script type="text/javascript" async src="assets/MathJax-3.2.2/es5/tex-chtml.js"> </script>\
+');
