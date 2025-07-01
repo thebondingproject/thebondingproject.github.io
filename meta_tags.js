5efb3d892c155508337ce5765130e6f2fa83bb81
thebondingproject <devs.thebondingproject@gmail.com>
2025-07-01 13:04:11 +0200

    fix meta again

--- meta_tags.js
+++ meta_tags.js
@@ -8,13 +8,12 @@ document.write('\
 <meta property="og:type" content="website">\
 <meta property="og:title" content="The Bonding Project">\
 <meta property="og:description" content="The Bonding Project aims at building a cost-effective bonding machine by repurposing the core of an old decommissioned industrial bonding machine using homemade and scrap parts.">\
-<meta property="og:image" content="https://opengraph.b-cdn.net/production/images/7dcbb4b3-b0fb-4153-bb8c-9cfc1ba8ae7a.jpg?token=-W7L8kqf0q_syLnRjYeWI6vw5ZB_rVhNzuRCpEWiStg&height=627&width=1200&expires=33287365812">\
+<meta property="og:image" content="https://thebondingproject.github.io/media/homepage/IMG-20240609-WA0008_meta.jpg">\
 <!-- Twitter Meta Tags -->\
 <meta name="twitter:card" content="summary_large_image">\
 <meta property="twitter:domain" content="thebondingproject.github.io">\
 <meta property="twitter:url" content="https://thebondingproject.github.io/">\
 <meta name="twitter:title" content="The Bonding Project">\
 <meta name="twitter:description" content="The Bonding Project aims at building a cost-effective bonding machine by repurposing the core of an old decommissioned industrial bonding machine using homemade and scrap parts.">\
-<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/7dcbb4b3-b0fb-4153-bb8c-9cfc1ba8ae7a.jpg?token=-W7L8kqf0q_syLnRjYeWI6vw5ZB_rVhNzuRCpEWiStg&height=627&width=1200&expires=33287365812">\
-<!-- Meta Tags Generated via https://www.opengraph.xyz -->\
+<meta name="twitter:image" content="https://thebondingproject.github.io/media/homepage/IMG-20240609-WA0008_meta.jpg">\
 ');
