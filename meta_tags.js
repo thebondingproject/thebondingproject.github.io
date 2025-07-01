6cf83df24c8fa6474faaaa72b9781050b8057a57
thebondingproject <devs.thebondingproject@gmail.com>
2025-07-01 12:31:32 +0200

    fix meta again

--- meta_tags.js
+++ meta_tags.js
@@ -1,7 +1,20 @@
 //Vedi "javascript - Include another HTML file in a HTML file - Stack Overflow.pdf"
 document.write('\
-	<meta property=’og:title’ content=The Bonding Project’/>\
-	<meta property=’og:image’ content=’//www.thebondingproject.github.io/media/homepage/IMG-20240609-WA0008_meta.jpg/>\
-	<meta property=’og:description’ content=’The Bonding Project aims at building a cost-effective bonding machine by repurposing the core of an old decommissioned industrial bonding machine using homemade and scrap parts.’/>\
-	<meta property=’og:url’ content=’//www.thebondingproject.github.io/index.html/>\
+<!-- HTML Meta Tags -->\
+<title>The Bonding Project</title>\
+<meta name="description" content="The Bonding Project aims at building a cost-effective bonding machine by repurposing the core of an old decommissioned industrial bonding machine using homemade and scrap parts.">\
+<!-- Facebook Meta Tags -->\
+<meta property="og:url" content="https://thebondingproject.github.io/">\
+<meta property="og:type" content="website">\
+<meta property="og:title" content="The Bonding Project">\
+<meta property="og:description" content="The Bonding Project aims at building a cost-effective bonding machine by repurposing the core of an old decommissioned industrial bonding machine using homemade and scrap parts.">\
+<meta property="og:image" content="https://opengraph.b-cdn.net/production/images/7dcbb4b3-b0fb-4153-bb8c-9cfc1ba8ae7a.jpg?token=-W7L8kqf0q_syLnRjYeWI6vw5ZB_rVhNzuRCpEWiStg&height=627&width=1200&expires=33287365812">\
+<!-- Twitter Meta Tags -->\
+<meta name="twitter:card" content="summary_large_image">\
+<meta property="twitter:domain" content="thebondingproject.github.io">\
+<meta property="twitter:url" content="https://thebondingproject.github.io/">\
+<meta name="twitter:title" content="The Bonding Project">\
+<meta name="twitter:description" content="The Bonding Project aims at building a cost-effective bonding machine by repurposing the core of an old decommissioned industrial bonding machine using homemade and scrap parts.">\
+<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/7dcbb4b3-b0fb-4153-bb8c-9cfc1ba8ae7a.jpg?token=-W7L8kqf0q_syLnRjYeWI6vw5ZB_rVhNzuRCpEWiStg&height=627&width=1200&expires=33287365812">\
+<!-- Meta Tags Generated via https://www.opengraph.xyz -->\
 ');
