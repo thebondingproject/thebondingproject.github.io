b4cc46b7665c2f6dfe23d4bdfdcfa2a0b7edd490
thebondingproject <devs.thebondingproject@gmail.com>
2025-07-01 12:25:11 +0200

    meta tags and manifest

---
+++ meta_tags.js
@@ -0,0 +1,7 @@
+//Vedi "javascript - Include another HTML file in a HTML file - Stack Overflow.pdf"
+document.write('\
+	<meta property=’og:title’ content=The Bonding Project’/>\
+	<meta property=’og:image’ content=’//media/homepage/IMG-20240609-WA0008_meta.jpg’/>\
+	<meta property=’og:description’ content=’The Bonding Project aims at building a cost-effective bonding machine by repurposing the core of an old decommissioned industrial bonding machine using homemade and scrap parts.’/>\
+	<meta property=’og:url’ content=’//https://thebondingproject.github.io/index.html/>\
+');
